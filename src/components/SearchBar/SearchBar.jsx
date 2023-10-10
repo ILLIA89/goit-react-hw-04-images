b7aa5191c2cc8e228139131d0bf0fdef49f088a6
ILLIA89 <anolij1@gmail.com>
2023-10-10 21:52:00 +0200

    SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,64 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Notiflix from 'notiflix';
 import styles from './SearchBar.module.css';
 
-class SearchBar extends Component {
-  state = {
-    name: '',
-  };
+const SearchBar = props => {
+  // state = {
+  //   name: '',
+  // };
+  const [name, setName] = useState('');
 
-  handleChange = event => {
+  const handleChange = event => {
     const { value } = event.currentTarget;
-    this.setState({ name: value });
+    setName(value);
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    if (this.state.name.trim() === '') {
+    if (name.trim() === '') {
       Notiflix.Notify.failure('search string is empty!');
       return;
     }
 
-    this.props.onSubmitHandler(this.state);
+    props.onSubmitHandler({ name });
 
-    this.reset();
+    reset();
   };
 
-  reset() {
-    this.setState({ name: '' });
-  }
+  const reset = () => {
+    setName('');
+  };
 
-  render() {
-    return (
-      <header className={styles.SearchBar}>
-        <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={styles.SearchFormButton}>
-            <span className={styles.SearchFormButtonLabel}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                viewBox="0 0 20 20"
-              >
-                <title>search</title>
-                <path d="M19 17l-5.15-5.15a7 7 0 1 0-2 2L17 19zM3.5 8A4.5 4.5 0 1 1 8 12.5 4.5 4.5 0 0 1 3.5 8z" />
-              </svg>
-            </span>
-          </button>
+  return (
+    <header className={styles.SearchBar}>
+      <form className={styles.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={styles.SearchFormButton}>
+          <span className={styles.SearchFormButtonLabel}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              viewBox="0 0 20 20"
+            >
+              <title>search</title>
+              <path d="M19 17l-5.15-5.15a7 7 0 1 0-2 2L17 19zM3.5 8A4.5 4.5 0 1 1 8 12.5 4.5 4.5 0 0 1 3.5 8z" />
+            </svg>
+          </span>
+        </button>
 
-          <input
-            className={styles.SearchFormInput}
-            type="text"
-            onChange={this.handleChange}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={styles.SearchFormInput}
+          type="text"
+          onChange={handleChange}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
 
 export default SearchBar;
